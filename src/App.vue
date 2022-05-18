378beeeac97b5c15b94b9fcba5365a34354a9c2d
Amarachi Iheanacho <amarachi2812@gmail.com>
2022-05-18 11:10:37 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,53 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h2>User Profile</h2>
+    <button @click="logout">Log out</button>
+    <button @click="login">Log in</button>
+    <pre v-if="isAuthenticated">
+      <code>{{ user }}</code>
+    </pre>
+  </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import { useAuth0 } from '@auth0/auth0-vue';
+  import {sdk} from '../init';
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  export default {
+    setup() {
+      const { loginWithRedirect, logout, user, isAuthenticated } = useAuth0();
+      return {
+        login: () => {
+          loginWithRedirect();
+          this.handleUserStorage()
+        },
+        user,
+        isAuthenticated,
+        logout: () => {
+          logout({ returnTo: window.location.origin });
+        }
+      }
+    },
+    watch: {
+      user: async function(){
+        const {nickname, name, email, picture } = this.user
+        if (this.isAuthenticated) {
+          alert('user saved')
+          try {
+            await sdk.database.createDocument('6284b56a769a6ac2a925', 'unique()', {
+              "nickname": nickname,
+              "name": name,
+              "email": email,
+              "picture": picture
+            })
+          } catch (error) {
+            console.log(error)
+          }
+          
+        } else {
+          return
+        }
+      }
+    }
+  };
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,17 @@
 import { createApp } from 'vue'
+
+import { createAuth0 } from '@auth0/auth0-vue';
+
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.use(
+  createAuth0({
+    domain: "dev-q6ylt640.us.auth0.com",
+    client_id: "J6jnlP8RpvkJhTCkmiL48v5EeFIhVvqv",
+    redirect_uri: window.location.origin
+  })
+);
+
+app.mount('#app');
